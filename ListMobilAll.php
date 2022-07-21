6d83f1b64b68c1e8e07a180da5501a159d71efbd
JinggaMustifaMaharaniRustam <Jinggamusfitaaa@gmail.com>
2022-07-21 14:16:54 +0700

    Penambahan Footer

--- ListMobilAll.php
+++ ListMobilAll.php
@@ -16,25 +16,27 @@
         $muat = $pisah[4];
         $mesin = $pisah[5];
 ?>   
-            <div class="col-md-3 col-sm-3">
-                <div class="card">
-                    <img src="<?php echo $gambar?>" alt="" class="card-img-top gambar-mobil img-fluid">
-                    <div class="card-body">
-                        <h2><?php echo $nama?> </h2>
-                        <p>Tipe: <?php echo $tipe?></p>
-                    </div>
-                    <div>
-                        <img src="assets/seats.png" alt=""> <?php echo $muat?> Orang
-                        <img src="assets/mesin.png" alt=""> <?php echo $mesin?> PS
-                    </div>
-                    <h4>Rp. <?php echo $harga?>/Hari </h4>
-                    <a href="Sewa.php?nama=<?php echo $nama?>&gambar=<?php echo $gambar?>&tipe=<?php echo $tipe?>&harga=<?php echo $harga?>">
-                        <button class="btn btn-success mb-3">Sewa Mobil</button>
-                    </a> 
+        <div class="col-md-3 col-sm-3">
+            <div class="card">
+                <img src="<?php echo $gambar?>" alt="" class="card-img-top gambar-mobil img-fluid">
+                <div class="card-body">
+                    <h2><?php echo $nama?> </h2>
+                    <p>Tipe: <?php echo $tipe?></p>
                 </div>
+                <div>
+                    <img src="assets/seats.png" alt=""> <?php echo $muat?> Orang
+                    <img src="assets/mesin.png" alt=""> <?php echo $mesin?> PS
+                </div>
+                <h4>Rp. <?php echo $harga?>/Hari </h4>
+                <a href="Sewa.php?nama=<?php echo $nama?>&gambar=<?php echo $gambar?>&tipe=<?php echo $tipe?>&harga=<?php echo $harga?>">
+                    <button class="btn btn-success mb-3">Sewa Mobil</button>
+                </a> 
             </div>
+        </div>
 <?php       
     }
 ?>
         </div>
     </div>
+
+<?php include "Footer.html" ?>
